78a59b69c81994e9df1e3ecbd9efdcb92eb93926
tariqsyed1 <trexrh@gmail.com>
2020-08-05 18:35:58 -0400

    added type.js and added proper indexing

---
+++ assets/js/type.js
@@ -0,0 +1,8 @@
+import Typed from 'typed.js';
+
+var options = {
+  strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+  typeSpeed: 40
+};
+
+var typed = new Typed('.element', options);

a1adf3806abea0c55a52e8cca8d054bc6f817af8
tariqsyed1 <trexrh@gmail.com>
2020-08-05 22:34:34 -0400

    changed type.js settings

--- assets/js/type.js
+++ assets/js/type.js
@@ -2,7 +2,7 @@ import Typed from './typedjs-master/src/typed.js';
 
 var options = {
   strings: ['TARIW', 'TARIQ'],
-  typeSpeed: 30
+  typeSpeed: 10
 };
 
 var typed = new Typed('#effect', options);

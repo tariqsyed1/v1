0f16f955f81d9377238feb8a629354ad758b6e90
tariqsyed1 <trexrh@gmail.com>
2020-08-06 17:24:09 -0400

    .

--- assets/js/grayscale.js
+++ assets/js/grayscale.js
@@ -39,4 +39,47 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
+  //Portfolio jquery
+  $(window).on('load', function() {
+    var portfolioIsotope = $('.portfolio-container').isotope({
+      itemSelector: '.portfolio-item'
+    });
+
+    $('#portfolio-flters li').on('click', function() {
+      $("#portfolio-flters li").removeClass('filter-active');
+      $(this).addClass('filter-active');
+
+      portfolioIsotope.isotope({
+        filter: $(this).data('filter')
+      });
+      aos_init();
+    });
+
+    // Initiate venobox (lightbox feature used in portofilo)
+    $(document).ready(function() {
+      $('.venobox').venobox({
+        'share': false
+      });
+    });
+  });
+
+  // Portfolio details carousel
+  $(".portfolio-details-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    items: 1
+  });
+
+  // Init AOS
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      once: true
+    });
+  }
+  $(window).on('load', function() {
+    aos_init();
+  });
+
 })(jQuery); // End of use strict

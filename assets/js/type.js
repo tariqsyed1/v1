cf7dfd33654ac7b581fe62088ef9fbfd92db0b91
tariqsyed1 <trexrh@gmail.com>
2020-08-05 23:38:15 -0400

    .

--- assets/js/type.js
+++ assets/js/type.js
@@ -2,11 +2,10 @@ import Typed from './typedjs-master/src/typed.js';
 
 var options = {
   strings: ['TARUQ', 'TARIQ'],
+  smartBackspace: true,
   typeSpeed: 200,
-  startDelay: 200,
   backSpeed: 200,
   backDelay: 200,
-  smartBackspace: true,
   showCursor: false,
 
 };
